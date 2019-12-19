22feb7e0cffcad46376fa511a60ca6d9c144fbde
Punnoose <punnoosekuttyj@yahoo.com>
2019-12-18 22:54:51 -0500

    added eureka server

--- currency-conversion/src/main/java/com/pk/currencyconversion/client/CurrencyExchangeResourceProxy.java
+++ currency-conversion/src/main/java/com/pk/currencyconversion/client/CurrencyExchangeResourceProxy.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service", url = "${CURRENCY_EXCHANGE_URI:http://localhost:8080}")
+@FeignClient(name = "currency-exchange-service")
 public interface CurrencyExchangeResourceProxy {
     @GetMapping("/rate/from/{from}/to/{to}")
     ExchangeRate fetchExchangeRate(@PathVariable("from") String from, @PathVariable("to") String to);
--- eureka-server/src/main/java/com/pk/eurekaserver/DemoApplication.java
+++ eureka-server/src/main/java/com/pk/eurekaserver/DemoApplication.java
@@ -1,17 +1,13 @@
-package com.pk.currencyconversion;
+package com.pk.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableFeignClients
-@EnableDiscoveryClient
+@EnableEurekaServer
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
